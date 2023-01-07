4ddbf9327c25a833878cb6e66e05a7bed0158208
alberto <guzmanmorenoalberto1998@gmail.com>
2023-01-07 19:04:38 +0100

    Vemos selectores para el HTML

--- cheatsheet/js/dom.js
+++ cheatsheet/js/dom.js
@@ -6,3 +6,18 @@ console.log(document.URL);
 document.title = "cheatsheet-js-Dom";
 console.log(document.title);
 //---- Selectors ---- 
+const family = document.getElementsByTagName("div");
+console.log(family);
+
+// Array.from(family).forEach(item => console.log(item));Construimos un array desde Html Collections
+// for (let familyMember of family) {
+//     console.log(familyMember);
+// }
+
+const grandParent = document.getElementById("grand-parent");
+console.log(grandParent);
+
+const parents = document.getElementsByClassName("parent");
+const parent1 = parents [0];
+const parent2 = parents [1];
+console.log(parent1,parent2);
