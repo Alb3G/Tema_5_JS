a25350955d9b9604286d996a3b36934e7f0bc686
alberto <guzmanmorenoalberto1998@gmail.com>
2023-01-18 14:40:54 +0100

    correcciones Ej sintaxis basica

--- ejercicios/Ejercicios-sintaxis/main.js
+++ ejercicios/Ejercicios-sintaxis/main.js
@@ -27,22 +27,19 @@ function isBoolean (arg) {
     //     return false;
     // }
 }
-let bool1 = 'true';
-let bool2 = true;
-let nobool = 5;
-console.log(isBoolean(bool1));
-console.log(isBoolean(bool2));
-console.log(isBoolean(nobool));
+console.log(isBoolean(true));
+console.log(isBoolean('true'));
+console.log(isBoolean(5));
 
 //4.Escribe una función que devuelva la longitud de un string recibido por argumento.
-const stringLength = (string) => console.log(`La longitud del string es de: ${string.length} caracteres`);
-stringLength('Hello World');
+const stringLength = (string) => `La longitud del string es de: ${string.length} caracteres`;
+console.log(stringLength('Hello World'));
 
 //5.Crea una función de flecha que reciba una cantidad de minutos y lo devuelva convertido en segundos.
-const minsToSecs = mins => console.log(`${mins} minutos son ${mins * 60} segundos`);
-minsToSecs(5);
+const minsToSecs = mins => `${mins} minutos son ${mins * 60} segundos`;
+console.log(minsToSecs(5));
 
-//6.Crea una función que reciba un número y devuelva el siguiente número par. (Si él es par, lo devolverá directamente).
+//6.Crea una función que reciba un número y devuelva el siguiente número par. (Si él es par, lo devolverá directamente).Se podria haber usado el operador ternario
 function square (number) {
     if (number % 2 === 0) {
         return number
@@ -53,14 +50,14 @@ function square (number) {
 console.log(square(5));
 
 //7. Crea una función que reciba una edad y devuelva la cantidad de días que esa persona ha vivido. Puedes obviar los años bisiestos.
-const personAge= (age) => {
+const personAge= (age) => {//Normalmente no vamos a haver returns de strings en las funciones simplemente devolveremos el calculo y en el .log pondremos el msj que queramos.
     if (age <= 0 || age > 100) {
-        console.log('La edad introducida no es correcta');
+        return 'La edad introducida no es correcta';
     }else {
-        console.log(`Teniendo ${age} años has vivido ${age * 365} dias`);
+        return `Teniendo ${age} años has vivido ${age * 365} dias`;
     }
 };
-personAge(24);
+console.log(personAge(24));
 
 //8. Crea una función que reciba un array y devuelva su último elemento. Prueba tu función con varios arrays de diferentes longitudes.
 function lastIndex(array) {
@@ -88,21 +85,20 @@ function countLegs (pollos, vacas, cerdos) {
 console.log(countLegs(5,2,8));
 
 //10. Crea una función que determine si dos datos recibidos por argumentos son del mismo tipo.
-const typeofFunction = (a, b) => {
-    if (typeof a === typeof b) {
-        console.log("Los datos son del mismo tipo");
-    } else {
-        console.log("Los datos son de distinto tipo");
-    }
-}
-typeofFunction(5,'5');
-
+// const typeofFunction = (a, b) => {
+//     if (typeof a === typeof b) {
+//         return "Los datos son del mismo tipo";
+//     } else {
+//         return "Los datos son de distinto tipo";
+//     }
+// }
+// console.log('Ejercicio 10: ',typeofFunction(5,'5'));
+const Ejercicio10 = (a,b) => typeof a === typeof b ? 'Los datos son del mismo tipo':'Los datos son de distinto tipo';
+console.log(Ejercicio10('5',5));
 //11. Crea una función que reciba un string con una frase y devuelva un array donde cada elemento será una
 //palabra de la frase original. Investigar método existente de los strings para este fin.
-function stringToArray(str) {
-    return str.split(" ");
-}
-let stringEjercicio11 = stringToArray("Esto es un string para probar la funcinon stringToArray");
+const stringToArray = (str) => str.split(" ");
+let stringEjercicio11 = stringToArray("Esto es un string para probar la funcion stringToArray");
 console.log(stringEjercicio11);
 
 //12. Inicializa dos objetos, address1 y address2 con las propiedades: provincia, ciudad, municipio, código postal, calle, número, planta, y número de puerta.
@@ -143,45 +139,47 @@ console.log(parseDomain('codespaceacademy.com'));
 //14. Nos han prohibido el uso del operador de igualdad estricta (===), pero queremos poder seguir utilizando
 //dicha funcionalidad. Crea una función que devuelva true si dos números tienen el mismo valor y el
 //mismo tipo de dato. Debemos usar el operador lógico “&&”.
-
-const equality = (firstNumber, secondNumber) => {
-    if (firstNumber == secondNumber && typeof firstNumber == typeof secondNumber) {
-        return true
-    } else{
-        return false
-    }
-}
-console.log(equality(5,5));
-console.log(equality('5',5));
+const equality = (num1,num2) => (num1 == num2) && (typeof num1 == typeof num2);//Esta forma es mejor para evitar redundancias con los valores booleanos.
+// const equality = (firstNumber, secondNumber) => {
+//     if (firstNumber == secondNumber && typeof firstNumber == typeof secondNumber) {
+//         return true
+//     } else{
+//         return false
+//     }
+// }
+console.log('Ejercicio 14: ',equality(5,5));
+console.log('Ejercicio 14: ',equality('5',5));
 
 //15. Crea una función que reciba dos strings y devuelva true si tienen la misma longitud y false en caso contrario.
-const lengthEquality = (str1, str2) => {
-    if (str1.length === str2.length) {
-        return true
-    }else {
-        return false
-    }
-}
-strEj15_1 = "Hello World";
-strEj15_2 = "Hola Mundo.";
-strEj15_3 = "Dam o Daw que estudiar?"
-console.log(`Ejercicio 15: ${lengthEquality(strEj15_1,strEj15_2)}`);
-console.log(`Ejercicio 15: ${lengthEquality(strEj15_1,strEj15_3)}`);
+// const lengthEquality = (str1, str2) => {
+//     if (str1.length === str2.length) {
+//         return true
+//     }else {
+//         return false
+//     }
+// }
+const lengthEquality = (str1,str2) => str1.length === str2.length;
+console.log(`Ejercicio 15: ${lengthEquality("Hello World","Hola Mundo.")}`);
+console.log(`Ejercicio 15: ${lengthEquality("Hello World","Dam o Daw que estudiar?")}`);
 
 //16. Crea una función que reciba un string y determine si está vacío sin utilizar la propiedad length.
-function emptyString (str) {
-    if (str === "") {
-        console.log('El string esta vacio');
-    }else {
-        console.log(`Contenido del String: ${str}`);
-    }
-}
-
+// function emptyString (str) {
+//     if (str === "") {
+//         console.log('El string esta vacio');
+//     }else {
+//         console.log(`Contenido del String: ${str}`);
+//     }
+// }
+// const emptyString = str => str.length === 0;
+const emptyString = str => str ? false : true;
+//La arrow de arriba nos sirve para entender el concepto de los falsy elements esto quiere decir que podemos poner en la parte de la condicion
+//el elemento como tal este lo validaremos con una condicion logica simplemente nombrandolo puesto que si tiene contenido nos dara true y si esta vacio
+//nos devolvera false esto pasa mucho con los [],{}," ".
 let stringVacio = "";
 let stringConContenido = "Hello World";
 
-emptyString(stringVacio);
-emptyString(stringConContenido);
+console.log('Ejercicio 16:',emptyString(stringVacio));
+console.log('Ejercicio 16:',emptyString(stringConContenido));
 
 //17. Imprimir elemento a elemento el array del apartado 1 de cuatro formas diferentes:
 //• for of
@@ -189,11 +187,12 @@ for (let item of stringArray) {
     console.log(`For of: ${item}`);
 }
 //• forEach.
-stringArray.forEach(function (value){
+function myFunction (value) {
     console.log(`For each: ${value}`);
-});
+}
+stringArray.forEach(myFunction);
 //• for
-for (let i = 0; i <= stringArray.length - 1; i++) {
+for (let i = 0; i < stringArray.length ; i++) {
     console.log(`For: ${stringArray[i]}`);
 }
 //• while
@@ -211,30 +210,34 @@ function repeatString(str, n) {//MDN Web Docs del parametro repeat para los stri
 console.log(repeatString(stringConContenido,3));
 console.log(repeatString('Me llamo Alberto ',3));
 //19. Crea una función que recibe un objeto con dos campos, votos positivos y votos negativos y que devuelva la cuenta final.
-const votos = (upVotes, downVotes) => console.log(`Los votos totales son: ${upVotes - downVotes}`);
-votos(35,15);
-
+function votes (obj) {
+    return obj.upVotes - obj.downVotes;
+}
+console.log(votes({upVotes: 35, downVotes: 15}));
 //20. Crea una función que recibe un array de tipos de datos mezclados y que devuelva otro array con el tipo de cada uno de los elementos.
 const mixArray = ["I'm learning JS in a Bootcamp 🚀", 7.5, {}, 0, undefined, [], "codespace"];
 function fillArray(array) {
     let newArray = [];
-    for (let i = 0; i < array.length; i++) {
-        newArray.push(typeof array[i]);
-    }
+    array.forEach(element => newArray.push(typeof element));
+    // for (let i = 0; i < array.length; i++) {
+    //     newArray.push(typeof array[i]);
+    // }
     return newArray;
 }
-fillArray(mixArray);
+
 console.log(fillArray(mixArray));
 
 //21. Función que dado un array de números con formato string devuelva un array con los números ya parseados.
 let arrayEj_21 = ['1','2','3','4','5'];
 function parsedNumbers (array) {
-    for (let i = 0; i < array.length; i++) {
-        array[i] = Number(array[i])//Le decimos que para cada indice del array que nos pasan vamos a sustituir ese indice por su mismo valor pero convertido a numero.
-    }
-    return array
+    let parsedArray = [];
+    array.forEach(element => parsedArray.push(Number(element)));
+    // for (let i = 0; i < array.length; i++) {
+    //     array[i] = Number(array[i])//Le decimos que para cada indice del array que nos pasan vamos a sustituir ese indice por su mismo valor pero convertido a numero.
+    // }
+    return parsedArray;
 }
-console.log(parsedNumbers(arrayEj_21));
+console.log('Ejercicio 21:',parsedNumbers(arrayEj_21));
 
 //22. Crea una función de flecha que devuelva “Positivo” si el número que recibe por argumento es mayor o igual que cero 
 //y “Negativo” en caso contrario. Usa el operador ternario.
@@ -245,20 +248,29 @@ console.log(ternaria(-1));
 //23. Crea una función que dado un array cualquiera y un índice, borre el elemento guardado en ese índice.
 function deleteIndexValue (array,index) {
     array.splice(index,1)
-    console.log(array);
+    return array;
 }
-
-deleteIndexValue(arrayEj_21,2)
+//const deleteIndexValue = (array,index) => array.splice(index,1);
+console.log('Ejercicio 23 :',deleteIndexValue(arrayEj_21,2));
 
 //24. Usando la función del apartado anterior, crea otra función que dado un array de números y un número a
 //filtrar, devuelva un array borrando todos las apariciones de dicho número.//Resuelto por chatGpt no cuenta.
-let filterArray = [2,3,4,3,6,3,8];
-function filterNumber(array, num) {
-    return array.filter(function(item) {
-        return item !== num;
-    });
-}
-console.log(filterNumber(filterArray,3));
+let filterArray = [2,3,3,4,3,6,3,8];
+// function filterNumber(array, num) {
+//     return array.filter(function(item) {
+//         return item !== num;
+//     });
+// }
+const filterNumber = (array, num) =>{
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === num) {
+            deleteIndexValue(array,i)
+            i--;
+        }
+    }
+    return array
+};
+console.log('Ejercicio 24:',filterNumber(filterArray,3));
 
 //25. Crea dos funciones que recibirán un objeto, la primera devolverá un array con los nombres de todas sus
 //propiedades. La segunda devolverá un array con los valores de dichas propiedades.
