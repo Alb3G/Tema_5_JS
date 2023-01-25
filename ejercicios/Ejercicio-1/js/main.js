1329ec67f54471444c2bb6e81293066b3bc34754
alberto <guzmanmorenoalberto1998@gmail.com>
2023-01-25 12:30:49 +0100

    correccion Ej1 apartado 9

--- ejercicios/Ejercicio-1/js/main.js
+++ ejercicios/Ejercicio-1/js/main.js
@@ -67,12 +67,34 @@ textArea.addEventListener("input", event => {
 //Para revertir el estado de una propiedad, podemos utilizar el valor “revert” o dejarla vacío.
 const squareButton = document.getElementsByTagName('button')[3];
 const squareInput = document.getElementById('squareInput')
-
-
 squareButton.addEventListener('click',() => {
     if (squareInput.value % 2 === 0){
-        squareInput.style.border = ' ';
+        squareInput.style.border = '2px solid green';
     }else{
         squareInput.style.border = '2px solid red';
     }
+});
+
+//7. Partiendo de una lista ul, crear 10 li con un texto indicando el número del elemento (“Elemento X”)
+//usando un bucle for.
+const ulAptado7 = document.getElementById('ulaptdo7');
+for (let i = 1; i <= 10; i++) {
+    const newLi = document.createElement('li');
+    newLi.textContent = `Elemento ${i}`;
+    ulAptado7.appendChild(newLi);
+}
+
+//8. Crear un enlace y un botón. Si pulso el enlace se me abre la URL en la misma página. Si pulso primero el
+//botón y luego el enlace, se me abre en una nueva pestaña.
+const aButton = document.getElementsByTagName('button')[4];
+const aFreepik = document.querySelector('#freepik')
+aButton.addEventListener('click',() => aFreepik.target = '_blank');//Tambien se puede hacer con el setAttribute pero esta forma es mas simple.
+
+//9. Añadir un párrafo y un select con 5 opciones de colores: negro, blanco, rojo, amarillo, verde y azul. Al
+//seleccionar un color del select, cambiar el color del párrafo.
+const select = document.querySelector('select');
+const colorParagraph = document.querySelector('#colorParagraph');
+
+select.addEventListener('select', (event) => {
+    colorParagraph.style.color = event.target.value;
 });
