3068826d1932d6fab6ef9c77f4e2268eedc4fe85
alberto <guzmanmorenoalberto1998@gmail.com>
2023-01-30 20:05:50 +0100

    Apartado 7 del Ej-2 Terminado

--- ejercicios/Ejercicio-2/js/main.js
+++ ejercicios/Ejercicio-2/js/main.js
@@ -1,11 +1,12 @@
-function user (name, firstLastName, secondlastName, email, age, city, cartProducts) {
+// Plantilla para crear Objetos.
+function user (name, firstLastName, secondlastName, email, age, city) {
     this.name = name;
     this.firstLastName = firstLastName;
     this.secondlastName = secondlastName;
     this.email = email;
     this.age = age;
     this.city = city;
-    this.cartProducts = cartProducts;
+    this.cartProducts = 0;
     this.incrementCartProducts = function () {
         this.cartProducts++;
     };
@@ -13,10 +14,40 @@ function user (name, firstLastName, secondlastName, email, age, city, cartProduc
         this.cartProducts = 0;
     }
 };
+// Array de Objetos creados con la plantilla.
 const users = [
-    new user ('Francisco','Perez', 'Perez','example1@hotmail.com',28,'Madrid',0),
-    new user ('Alejandro','Gomez', 'Miranda','example2@outlook.com',40,'Madrid',0),
-    new user ('Francisco','Perez', 'Perez','example3@gmail.es',19,'Madrid',0),
+    new user ('Francisco','Perez', 'Ruiz','example1@hotmail.com',28,'Madrid'),
+    new user ('Alejandro','Gomez', 'Miranda','example2@outlook.com',40,'Madrid'),
+    new user ('Manuela','Jimenez', 'Pajuelo','example3@gmail.es',19,'Madrid'),
 ];
-
+// ForEach usado para seleccionar solamente los emails de los usuarios.
 users.forEach(user => console.log(user.email));
+// Seleccionamos los elemetos en el Dom y guardamos sus referencias.
+const userSelect = document.querySelector('#userselect');//Select.
+const userDataList = document.querySelector('#userData');//Ul.
+const addToCartButton = document.querySelector('#addProduct');//Add products Button.
+const emptyCartProductsButton = document.querySelector('#emptyCart');//Empty cart for selected user.
+// Add Nombre de los usuarios al Select iterando sobre el array.
+users.forEach((user) => {
+    let newOption = document.createElement('option');
+    newOption.textContent = user.name;
+    userSelect.appendChild(newOption);
+});
+//Rellenamos la lista con todas las propiedades de un usuario cualquiera.
+function fillList (user) {
+    userDataList.innerHTML = " ";
+    for (const key in user) {
+        const value = user[key];
+        if (typeof value !== 'function'){
+            let newLi = document.createElement('li');
+            newLi.innerHTML = `<b> ${key}:</b> ${value}`;
+            newLi.classList.add('list-group-item');
+            userDataList.appendChild(newLi);
+        }
+    };
+}
+//Añadir listeners necesarios.
+userSelect.addEventListener('change',(event) => {
+    const selectedUser = users.find(user => user.name === userSelect.value);
+    fillList(selectedUser);
+});
