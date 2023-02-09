58ab69787f634d7f23b8a87e3e0cbe27463c2f9e
alberto <guzmanmorenoalberto1998@gmail.com>
2023-02-09 12:38:50 +0100

    Ejercicio-4 Empezado

--- ejercicios/Ejercicio-4/js/main.js
+++ ejercicios/Ejercicio-4/js/main.js
@@ -1,1 +1,37 @@
-console.log('hello world');
+// Constructor del Obj Book
+function book (id, title, author, sales, price) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.sales = sales;
+    this.price = price;
+};
+// Array of 10 books
+let library = [
+    new book (1,'Cien años de Soledad','Gabriel Garcia Marquez',740120,20),
+    new book (2,'Orgullo y Prejuicio','Jane Austen',678234,15),
+    new book (3,'1984','George Orwel',239876,19.99),
+    new book (4,'Odisea','Homero',851257,35),
+    new book (5,'Lolita','Vladimir Nabokov',134589,9.99),
+    new book (6,'Cumbres Borrascosas','Emily Bronte',256435,20),
+    new book (7,'Moby-Dick','Herman Melville',871312,18),
+    new book (8,'Las mil y una Noches','Mario Vargas Llosa',543879,25),
+    new book (9,'Guerra & Paz','Leon Tolstoi',712532,39.99),
+    new book (10,'Grandes Esperanzas','Charles Dickens',913456,29.95),
+];
+// Select the Dom elements.
+const booksTable = document.querySelector('#tbody');
+// Function to create the content of the table.
+function fillTable (book) {
+    let newTr = document.createElement('tr');
+    newTr.innerHTML =  `<tr>
+                            <th>${book.id}</th>
+                            <td>${book.title}</td>
+                            <td>${book.author}</td>
+                            <td>${book.sales} €</td>
+                            <td>${book.price} €</td>
+                            <td><button type="button" class="btn btn-danger">Remove</button></td>
+                        </tr>`;
+    booksTable.appendChild(newTr);
+};
+library.forEach(fillTable);
