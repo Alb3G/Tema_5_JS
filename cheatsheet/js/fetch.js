48ba673cfae445abe0864a576cfb44d28dbabd6d
Alberto <guzmanmorenoalberto1998@gmail.com>
2023-02-27 12:22:35 +0100

    Mandamos datos a la API con el metodo POST

--- cheatsheet/js/fetch.js
+++ cheatsheet/js/fetch.js
@@ -32,3 +32,34 @@ fetch(URL)
 //
 // promise.then(response => console.log(response)); //El .then() se aplica sobre promesas y se ejecutara cuando se resuelva la promesa.
 // console.log(promise);//Esta linea se ejecutara antes que la de la promesa.
+
+// Async && Await
+async function requestURL (URL) {
+    console.log(2);
+    const response = await fetch(URL);
+    const data = await response.json()
+    console.log(data);
+    console.log(4);
+};
+console.log(1);
+requestURL(URL);
+console.log(3);
+
+// Peticion con metodo HTTP en lugar de GET.
+const POST_URL = "https://jsonplaceholder.typicode.com/posts";
+const newPost = {
+    title: "Code Space Alumn",
+    body: "Future Software Developer"
+}
+const http = {
+    method:"POST",
+    headers: {
+        "content-type": "application/json"
+    },
+    body :JSON.stringify(newPost)
+};
+
+fetch(POST_URL,http)
+    .then(response => response.json())
+    .then(data => console.log(data));
+//
