c6fe934db7c898e99da612af1adac3d632f85b1b
Alberto <guzmanmorenoalberto1998@gmail.com>
2023-02-27 20:54:54 +0100

    Avance con la paginacion del array.

--- ejercicios/Ejercicio fetch(posts)/js/fetch.js
+++ ejercicios/Ejercicio fetch(posts)/js/fetch.js
@@ -1,7 +1,29 @@
-// Esta parte es para traerme la info de los posts.
+// Esta parte es para traerme la info de los posts y los selectores.
 const URL = "https://jsonplaceholder.typicode.com/posts";
 const dataDiv = document.querySelector('#data');
-// const body = document.getElementsByTagName('body')[0]; he quitado el body para que los botones vieran abajo del todo.
+const prevButton = document.querySelector('#prevButton');
+const nextButton = document.querySelector('#nextButton');
+const page = document.querySelector('#page');
+// variables 
+let pageNumber = 1;
+let postPerPage = 20;
+// Functions.
+async function requestURL (URL) {
+    const response = await fetch(URL);
+    const data = await response.json();
+    createPosts(data);
+    console.log(data);
+    
+    nextButton.addEventListener('click', () => {
+        data.forEach(user => {
+            pageNumber++;
+            page.innerHTML = pageNumber;
+            createPosts(paginateArray(data, pageNumber, postPerPage));
+        });
+    });
+};
+requestURL(URL);
+
 function createPosts (array) {
     array.slice(1,20).forEach(user => {
         dataDiv.innerHTML += `<h1>${user.title}</h1>
@@ -9,17 +31,14 @@ function createPosts (array) {
     });
 };
 
-async function requestURL (URL) {
-    const response = await fetch(URL);
-    const data = await response.json();
-    createPosts(data);
+function paginateArray(array, pageNumber, pageSize)  {
+    const page = array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
+    return page;
 };
-requestURL(URL);
 
-function paginateArray (array,pageSize,pageNumber) {
 
-}
+
+
+
 
 // seleccion de los botones.
-const prevButton = document.querySelector('#prevButton');
-const nextButton = document.querySelector('#nextButton');
